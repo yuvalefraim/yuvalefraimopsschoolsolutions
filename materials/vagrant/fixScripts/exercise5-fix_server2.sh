9d0301687ff939d361435a85afc1ffd853d4c277
yuvalefraim <145375601+yuvalefraim@users.noreply.github.com>
2023-09-18 19:44:24 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
-#!/bin/bash
-#add fix to exercise5-server2 here
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 #service sshd restart
 
